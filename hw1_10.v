79cd136e1cf87b2b695dbb9970db6c211edf336d
Yasirul Choudhury <018189249@coe-ee-cad30.sjsuad.sjsu.edu>
2026-04-25 19:53:01 -0700

    cleaned up formatting

--- hw1_10.v
+++ hw1_10.v
@@ -1,64 +1,37 @@
 // Generated HW1 Spring'26 assignment hw1_10
 
-module hw1_10(input clk, input reset,input pushin,
-        input logic signed [32:0] M00,
-        input logic signed [32:0] M01,
-        input logic signed [32:0] M02,
-        input logic signed [32:0] M03,
-        input logic signed [32:0] M04,
-        input logic signed [32:0] M05,
-        input logic signed [32:0] M06,
-        input logic signed [32:0] M07,
-        input logic signed [32:0] M08,
-        input logic signed [32:0] M09,
-        input logic signed [32:0] M10,
-        input logic signed [32:0] M11,
-        input logic signed [32:0] C00,
-        input logic signed [32:0] C01,
-        input logic signed [32:0] C02,
-        input logic signed [32:0] C03,
-        input logic signed [32:0] C04,
-        input logic signed [32:0] C05,
-        input logic signed [32:0] C06,
-        input logic signed [32:0] C07,
-        input logic signed [32:0] C08,
-        input logic signed [32:0] C09,
-        input logic signed [32:0] C10,
-        input logic signed [32:0] C11,
-        input logic signed [65:0] D00,
-        input logic signed [65:0] D01,
-        input logic signed [65:0] D02,
-        input logic signed [65:0] D03,
-        input logic signed [65:0] D04,
-        input logic signed [65:0] D05,
-        input logic signed [65:0] D06,
-        input logic signed [65:0] D07,
-        output logic pushout,output signed [74:0] rd);
-    reg signed [32:0] ML00,CL00;
-    reg signed [32:0] ML01,CL01;
-    reg signed [32:0] ML02,CL02;
-    reg signed [32:0] ML03,CL03;
-    reg signed [32:0] ML04,CL04;
-    reg signed [32:0] ML05,CL05;
-    reg signed [32:0] ML06,CL06;
-    reg signed [32:0] ML07,CL07;
-    reg signed [32:0] ML08,CL08;
-    reg signed [32:0] ML09,CL09;
-    reg signed [32:0] ML10,CL10;
-    reg signed [32:0] ML11,CL11;
-    reg signed [65:0] DL00;
-    reg signed [65:0] DL01;
-    reg signed [65:0] DL02;
-    reg signed [65:0] DL03;
-    reg signed [65:0] DL04;
-    reg signed [65:0] DL05;
-    reg signed [65:0] DL06;
-    reg signed [65:0] DL07;
-    reg signed [74:0] rd_d,rdi;
-    reg push1,pusho;
-    assign rd=rdi;
-    assign pushout=pusho;
-    always @(*) begin
+module hw1_10(
+	input clk, 
+	input reset,
+	input pushin,
+        input logic signed [32:0] M00, M01, M02, M03, M04, M05, M06, M07, M08, M09, M10, M11,
+        input logic signed [32:0] C00, C01, C02, C03, C04, C05, C06, C07, C08, C09, C10, C11,
+        input logic signed [65:0] D00, D01, D02, D03, D04, D05, D06, D07,
+        
+	output logic pushout,output signed [74:0] rd);
+   	reg signed [32:0] ML00,CL00;
+        reg signed [32:0] ML01,CL01;
+        reg signed [32:0] ML02,CL02;
+        reg signed [32:0] ML03,CL03;
+        reg signed [32:0] ML04,CL04;
+        reg signed [32:0] ML05,CL05;
+        reg signed [32:0] ML06,CL06;
+        reg signed [32:0] ML07,CL07;
+        reg signed [32:0] ML08,CL08;
+        reg signed [32:0] ML09,CL09;
+        reg signed [32:0] ML10,CL10;
+        reg signed [32:0] ML11,CL11;
+        reg signed [65:0] DL00, DL01, DL02, DL03, DL04, DL05, DL06, DL07;
+    
+        reg signed [74:0] rd_d,rdi;
+        
+	reg push1,pusho;
+        
+	assign rd=rdi;
+        assign pushout=pusho;
+        
+	
+	always @(*) begin
         rd_d=ML00*(ML11<CL03?CL02:CL00)+((ML02>=ML09?ML03:ML04)*(ML04>CL06?CL01:ML05)+DL07)-((ML08<=ML00?ML10:CL05)*(CL07>ML01?ML02:ML08)+DL05)-(ML10<ML05?ML08:ML02)*CL03+(ML11<CL03?CL02:CL00)^CL04+((ML02>=ML09?ML03:ML04)*(ML04>CL06?CL01:ML05)+DL06)+(ML06*(ML08<=ML00?ML10:CL05)+DL02)+ML07*(CL07>ML01?ML02:ML08)-((ML10<ML05?ML08:ML02)*CL08+DL03)-((ML11<CL03?CL02:CL00)*CL09+DL01)+((ML02>=ML09?ML03:ML04)*CL10+DL04)+((ML04>CL06?CL01:ML05)*CL11+DL00);
     end
     always @(posedge(clk) or posedge(reset)) begin
