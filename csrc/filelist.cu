2fc81645b6d4c86efd3d797285b94be0823138bc
Yasirul Choudhury <018189249@coe-ee-cad30.sjsuad.sjsu.edu>
2026-04-25 17:59:30 -0700

    84.5 MHz

--- csrc/filelist.cu
+++ csrc/filelist.cu
@@ -4,12 +4,12 @@ ifeq ($(LDVERSION), 0)
 endif
 
 ARCHIVE_OBJS=
-ARCHIVE_OBJS += _879140_archive_1.so
-_879140_archive_1.so : archive.0/_879140_archive_1.a
+ARCHIVE_OBJS += _886231_archive_1.so
+_886231_archive_1.so : archive.0/_886231_archive_1.a
 	@$(AR) -s $<
-	@$(PIC_LD) -shared  -z notext -m elf_i386  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_879140_archive_1.so --whole-archive $< --no-whole-archive
+	@$(PIC_LD) -shared  -z notext -m elf_i386  -Bsymbolic $(LD_NORELAX_FLAG)  -o .//../simv.daidir//_886231_archive_1.so --whole-archive $< --no-whole-archive
 	@rm -f $@
-	@ln -sf .//../simv.daidir//_879140_archive_1.so $@
+	@ln -sf .//../simv.daidir//_886231_archive_1.so $@
 
 
 
